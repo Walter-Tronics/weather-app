e47bd65099371bf864e566bf33bf7aa802e5b2de
Fatemeh_kamarei <79019952+fateme25@users.noreply.github.com>
2023-01-24 23:09:01 +0330

    Update time every second

--- src/index.js
+++ src/index.js
@@ -36,7 +36,9 @@ function showDate(date) {
 function searchWeather(location) {
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&units=metric`;
   axios.get(weatherUrl + "&" + location).then((response) => {
-    date.innerHTML = showDate(new Date());
+        setInterval(()=>{
+      date.innerHTML = showDate(new Date());
+    },1000);
     place.innerHTML = response.data.name;
     weatherCondition.innerHTML = response.data.weather[0].main;
     temperature.innerHTML = Math.round(response.data.main.temp);
